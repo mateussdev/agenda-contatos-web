4aa0fb9d99db8f569c9c270c9a6cfddd246a5add
Mateus Silva <mateusbs427@gmail.com>
2023-10-24 09:25:53 -0300

    database connection

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.CONNECTIONSTRING)
+  .then(() => {
+    app.emit('ready');
+  })
+  .catch(e => console.log(e));
 
 const path = require('path');
 
@@ -8,12 +17,16 @@ const routes = require('./routes');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static(path.resolve(__dirname, 'public')));
+
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
 app.use(routes);
 
-app.listen('4000', () => {
-  console.log('Servidor rodando na porta 4000.')
-  console.log('Acessar http://localhost:4000');
-});
+app.on('ready', () => {
+  app.listen('4000', () => {
+    console.log('Servidor rodando na porta 4000.')
+    console.log('Acessar http://localhost:4000');
+  });
+})
