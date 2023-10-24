a08597cef2de567e2501932468bff8813a7c0424
Mateus Silva <mateusbs427@gmail.com>
2023-10-24 09:28:53 -0300

    login and register pages

--- routes.js
+++ routes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const route = express.Router();
 
 const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
 
 route.get('/', homeController.index);
+
+route.get('/login', loginController.login);
+route.get('/register', loginController.register);
 
 module.exports = route;
