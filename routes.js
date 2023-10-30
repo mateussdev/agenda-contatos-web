0bde071f5ee8b6f2aa821aa8e9334b04ee68b12b
Mateus Silva <mateusbs427@gmail.com>
2023-10-30 09:51:39 -0300

    login required to access contacts

--- routes.js
+++ routes.js
@@ -6,6 +6,8 @@ const loginController = require('./src/controllers/loginController');
 const registerController = require('./src/controllers/registerController');
 const contactController = require('./src/controllers/contactController');
 
+const { loginRequired } = require('./src/middlewares/middleware');
+
 route.get('/', homeController.index);
 
 route.get('/login', loginController.index);
@@ -14,7 +16,7 @@ route.get('/register', registerController.index);
 route.post('/register/registed', registerController.register);
 route.get('/logout', loginController.logout);
 
-route.get('/contact', contactController.index);
-route.get('/contact/register', contactController.register);
+route.get('/contact', loginRequired, contactController.index);
+route.get('/contact/register', loginRequired, contactController.register);
 
 module.exports = route;
