10894fdf6345b4da1bdebfa8fbb872fe78001697
Mateus Silva <mateusbs427@gmail.com>
2023-11-03 10:56:03 -0300

    frontend test

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -3,7 +3,7 @@ var __webpack_exports__ = {};
 /*!**************************!*\
   !*** ./frontend/main.js ***!
   \**************************/
-console.log('Hello, world!');
+console.log('Hello, world');
 /******/ })()
 ;
 //# sourceMappingURL=bundle.js.map
